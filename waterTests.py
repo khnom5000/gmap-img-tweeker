022720773cefc020fec87bda09ce5d36a827f40b
Red C <redford.crawford@gmail.com>
2024-03-09 19:58:24 +0000

    tidy up waterTests

--- waterTests.py
+++ waterTests.py
@@ -1,7 +1,5 @@
 from PIL import Image
 import numpy as np
-# from matplotlib import interactive
-# interactive(True)
 from  matplotlib import pyplot as plt
 from math import sqrt
 #import dev_to_kmeans_article as dtkm
@@ -10,76 +8,21 @@ import jonchar_net_article as jcna
 water_removal_ratio = 20000
 
 blue = [156,192,249]
-
-#im = Image.open('3.png').convert('RGB')
-#im = Image.open('t.png').convert('RGB')
-#im = Image.open('t2.png').convert('RGB')
-#im = Image.open('t2_90.png').convert('RGB')
-#im = Image.open('t3.png').convert('RGB')
-#im = Image.open('t3_2w_c.png').convert('RGB')
 im = Image.open('t3_2w_c_bw.png').convert('RGB')
-
 imnp = np.array(im)
-
 #print(imnp.shape)
-
-#set up some stuff foe later
-# print("height:",len(imnp[:,0]),"width:",len(imnp[0]))
-# print("size:",len(imnp[:,0])*len(imnp[0]))
-# print("water body pixel limit:",(len(imnp[:,0])*len(imnp[0]))/water_removal_ratio)
-#limit=(len(imnp[:,0])*len(imnp[0]))/water_removal_ratio
 
 #x here holds a tuple - of line, column, RBG value, we can ignore the RBG bit i.e. x[2] as its always [0,1,2]
 x = np.where(imnp == blue)
-# print(x)
-#this will tell us how "big" the water is if we uniq it down i.e. there are X pixels of "water"
-#print("row:",x[0],"col:",x[1])
 
+#Get the uniq list of "blue" pixels
 water_list = list()
 for i in range(len(x[0])):
     water_list.append([x[0][i],x[1][i]])
 all_water = [list(x) for x in set(tuple(x) for x in water_list)]
-# print(all_water)
-# print(type(all_water))
+
+#make it np
 X=np.asarray(all_water)
-# print(type(X))
-
-# print(np.linalg.norm(all_water[0])-all_water[1])
-# print(np.linalg.norm(all_water[1])-all_water[0])
-# print(np.linalg.norm(all_water[0])-all_water[2])
-# print(np.linalg.norm(all_water[2])-all_water[0])
-
-##HOW DO WE GROUP BODIES OF WATER ?? for now we hack :)
-#water_group = all_water
-##provided we can sort the above issue... we can continue to "fix" it
-
-#if len(water_group) < limit:
-#    print("water group is to small - removing:",water_group)
-
-#print(x)
-#x_2d = np.delete(x,2,0)
-#print(x_2d)
-#x_uniq = np.unique(x_2d,axis=1)
-# print(x_uniq.shape)
-# x_sort = np.sort(x_uniq,axis=1)
-# print(x_sort)
-#all_water_stacked = np.stack(all_water, axis=1)
-# print(all_water_stacked)
-#all_water_stacked_back = np.stack(all_water, axis=0)
-# print(all_water_stacked_back)
-   
-# plt.plot(all_water)
-
-# for water in all_water:
-#     print(water)
-#     print(sqrt((water[0]*water[0])+(water[1]*water[1])))
-
-# grouped_water = []
-# while len(all_water) != 0:
-#     print(all_water)
-#     water = all_water.pop(0)
-#     print(water)
-#     print(all_water)
 
 """dev_to_kmeans_article https://dev.to/sajal2692/coding-k-means-clustering-using-python-and-numpy-fg1"""
 # clusters, centroids=dtkm.run_Kmeans(5,X)
